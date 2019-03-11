2aa12b976c040f99c231052e5e31b1dc31eb71b7
Jordan Begian <jordanbegian@Jordans-MacBook-Pro.local>
2019-03-11 15:26:33 -0400

    add variables to BreweryDetails with setters and getters // Add @JsonIgnoreProperties to BreweryDetails and BreweryList

--- src/main/java/com/detroitlabs/brewskidoodle/Model/BreweryDetails.java
+++ src/main/java/com/detroitlabs/brewskidoodle/Model/BreweryDetails.java
@@ -1,4 +1,70 @@
 package com.detroitlabs.brewskidoodle.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BreweryDetails {
+    private String name;
+    private String street;
+    private String city;
+    private String state;
+    private String postalCode; //TODO JSON Property postal_code
+    private String phone;
+    private String websiteUrl; //TODO JSON Property website_url
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getWebsiteUrl() {
+        return websiteUrl;
+    }
+
+    public void setWebsiteUrl(String websiteUrl) {
+        this.websiteUrl = websiteUrl;
+    }
 }
--- src/main/java/com/detroitlabs/brewskidoodle/Model/BreweryList.java
+++ src/main/java/com/detroitlabs/brewskidoodle/Model/BreweryList.java
@@ -1,4 +1,8 @@
 package com.detroitlabs.brewskidoodle.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BreweryList {
+
 }
